b5f7ba37a3600f8f633e2d647623b603b1336b39
Sergey Denisovich <densergej2016@gmail.com>
2021-03-03 10:35:13 +0300

    add react-redux & redux/toolkit in project

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './scss/app.scss';
 import 'antd/dist/antd.css';
 import App from './App';
+import store from './app/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
